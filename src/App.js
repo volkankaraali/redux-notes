8cdefb8b736d2681bd5658d67e8ca4261cdd9fac
volkan karaali <volkankaraali1@gmail.com>
2021-12-20 18:13:19 +0300

    githublink added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import NoteForm from './components/NoteForm';
 import NoteList from './components/NoteList';
 import { store } from './redux/store';
 import Search from './components/Search';
+import { Text } from '@mantine/core';
 
 function App() {
   return (
     <Provider store={store}>
+      <div className='githubLink'>
+        <a href='https://github.com/volkankaraali' target="_blank"><Text size="lg">github.com/volkankaraali</Text></a>
+      </div>
       <NoteForm />
       <Search />
       <NoteList />
